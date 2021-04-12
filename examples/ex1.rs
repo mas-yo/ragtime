acb2d08decbde18ca61fabb615536b3a643b7b9d
Masahiro Yoshida <mypianoplayer@gmail.com>
2021-04-12 00:42:03 +0000

    basic usase of start_tcp

--- examples/ex1.rs
+++ examples/ex1.rs
@@ -1,12 +1,15 @@
 use bytes::BytesMut;
 use core::future::Future;
-use tokio_stream::{self as stream, StreamExt};
-use tokio_util::codec::{Decoder, Encoder};
+use tokio::net::TcpStream;
+use tokio_stream::{self as stream, Stream, StreamExt};
+use tokio_util::codec::{Decoder, Encoder, Framed};
+
+type RpcMessage = i32;
 
 #[derive(Default)]
 struct Ex1Decoder {}
 impl Decoder for Ex1Decoder {
-    type Item = i32;
+    type Item = RpcMessage;
     type Error = std::io::Error;
 
     fn decode(&mut self, src: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
@@ -14,7 +17,7 @@ impl Decoder for Ex1Decoder {
     }
 }
 struct Ex1Encoder {}
-impl Encoder<i32> for Ex1Encoder {
+impl Encoder<RpcMessage> for Ex1Encoder {
     type Error = std::io::Error;
 
     fn encode(&mut self, item: i32, dst: &mut BytesMut) -> Result<(), Self::Error> {
@@ -22,19 +25,30 @@ impl Encoder<i32> for Ex1Encoder {
     }
 }
 
-fn test<S>(mut stream: S) -> impl Future<Output = ()>
+async fn game<S>(mut stream: S) -> Result<(), Box<dyn std::error::Error>>
 where
-    S: StreamExt + Unpin,
+    S: Stream<Item = Result<RpcMessage, std::io::Error>> + Unpin,
 {
-    async move {
-        stream.next().await;
+    loop {
+        match stream.next().await {
+            Some(m) => {
+                // let i:u32 = m;
+            }
+            None => {}
+        }
     }
 }
 
 fn main() {
+    async fn game_(stream: Framed<TcpStream, Ex1Decoder>) {
+        game(stream).await.unwrap_or_else(|err| {
+            println!("{}", err);
+        });
+    }
+
     tokio::runtime::Builder::new_multi_thread()
         .enable_all()
         .build()
         .unwrap()
-        .block_on(ragtime::start_tcp::<Ex1Decoder, _>(test));
+        .block_on(ragtime::start_tcp(game_));
 }
